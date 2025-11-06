5e05b2d91151229ea24389465a39e8ef3f2dd7c0
NabeelElraf <NabeelHE123@gmail.com>
2025-11-06 14:19:26 +0700

    questionExperiment1

--- SIakadFor18.java
+++ SIakadFor18.java
@@ -7,20 +7,26 @@ public class SIakadFor18 {
 
  
         double grade, highest = 0, lowest = 100;
+        int pass = 0, fail = 0;
 
         for (int i = 1; i <= 10; i++) {
-            System.out.print("Enter the grade of the " + i + " student;");
+            System.out.print("Enter the grade of the " + i + " student:");
             grade = sc.nextDouble();
+
             if (grade > highest) {
                 highest = grade;
             }
             if (grade < lowest) {
                 lowest = grade;
             }
+            if (grade >= 60) pass++;
+            else fail++;
         }
 
         System.out.println("The highest grade: " + highest);
         System.out.println("THe lowest grade: " + lowest);
+        System.out.println("Students passed: " + pass);
+        System.out.println("Students failed: " + fail);
 
     }
 
